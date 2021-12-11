4742bcbb4cd1e2ecd85ddbd8985490300f329e13
Chengen-Cai <61627481+Chengen-Cai@users.noreply.github.com>
2021-12-11 21:45:31 +0800

    第四次更新

--- testtestshow.cpp
+++ testtestshow.cpp
@@ -4,29 +4,5 @@
 using namespace std;
 
 int main(){
-    char input;
-    while (1)
-    {
-        cin>>input;
-        char b;
-        if ( !((input>='a'&input<='z')|(input>='A'&input<='Z')) )
-        {
-            continue;
-        }
-        if ((input>='a'&input<='z'))
-        {
-            b = (a-'a'+'A');
-            cout<<b;
-        }
-        else if(input>='A'&input<='Z')
-        {
-            b = (input-'A'+'a');
-            cout<<b;
-        }
-        
-    }
-    2
-    A B C
     
-    a b c
 }
